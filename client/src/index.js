e9185639b28751b7d60d7ccec163a811380d3dd9
dansirdan <daniel.monteton@gmail.com>
2019-08-17 16:29:52 -0600

    cleaning files

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import * as serviceWorker from './serviceWorker';
 
-render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
